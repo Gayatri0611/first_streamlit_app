671d2be8f210fe4f42a8966fbc1d1f19ffc59632
Gayatri0611 <103627891+Gayatri0611@users.noreply.github.com>
2022-05-12 19:15:13 +0530

    Update streamlit_app.py
    added dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,8 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 and blueberry oatmeal')
 streamlit.text('Pav bhaji')
 
+
+import pandas as pd
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+streamlit.title('build your own fruit smoothieee')
