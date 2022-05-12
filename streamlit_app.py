4a3bdff6c732d947ad68dfe1b8730f97024c1269
Gayatri0611 <103627891+Gayatri0611@users.noreply.github.com>
2022-05-12 19:16:25 +0530

    Update streamlit_app.py
    removed alias error

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ streamlit.text('Omega 3 and blueberry oatmeal')
 streamlit.text('Pav bhaji')
 
 
-import pandas as pd
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 streamlit.title('build your own fruit smoothieee')
