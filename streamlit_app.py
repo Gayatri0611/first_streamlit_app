610e142c749931ca622bb0d8c38d832c9c03cb78
Gayatri0611 <103627891+Gayatri0611@users.noreply.github.com>
2022-05-12 19:01:42 +0530

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.title('Breakfast Menu')
+streamlit.title('Omega 3 and blueberry oatmeal')
+#streamlit.title('My Parents New Healthy Diner')
+
