ca21c7d4cc4a8960052e9d4e653a20256193474f
Gayatri0611 <103627891+Gayatri0611@users.noreply.github.com>
2022-05-12 19:17:21 +0530

    Update streamlit_app.py
    changed the sequence of sentences

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 and blueberry oatmeal')
 streamlit.text('Pav bhaji')
 
-
+streamlit.title('build your own fruit smoothieee')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
-streamlit.title('build your own fruit smoothieee')
+
